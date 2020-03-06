5e47b39ee5968847bf6f27a711aec0926268245a
Stephen Gardner <stephenbgardner@gmail.com>
2020-03-06 15:55:19 -0800

    Add Skills

---
+++ ftobj/skills.go
@@ -0,0 +1,63 @@
+package ftobj
+
+import (
+	"context"
+	"intra/ftapi"
+	"strconv"
+)
+
+type (
+	Skill struct {
+		req       ftapi.RequestData
+		ID        int        `json:"id"`
+		Name      string     `json:"name"`
+		CreatedAt ftapi.Time `json:"created_at"`
+	}
+	Skills struct {
+		req        ftapi.RequestData
+		Collection []Skill
+	}
+	SkillCUParams struct {
+		Name string `json:"name,omitempty"`
+	}
+)
+
+func (skill *Skill) Create(ctx context.Context, params SkillCUParams) ftapi.CachedRequest {
+	skill.req.Endpoint = ftapi.GetEndpoint("skills", nil)
+	skill.req.ExecuteMethod = func() {
+		skill.req.Create(ctx, skill, ftapi.EncapsulatedMarshal("skill", params))
+	}
+	return &skill.req
+}
+
+func (skill *Skill) Delete(ctx context.Context) ftapi.Request {
+	skill.req.Endpoint = ftapi.GetEndpoint("skills/"+strconv.Itoa(skill.ID), nil)
+	skill.req.ExecuteMethod = func() {
+		skill.req.Delete(ctx, skill)
+	}
+	return &skill.req
+}
+
+func (skill *Skill) Patch(ctx context.Context, params SkillCUParams) ftapi.Request {
+	skill.req.Endpoint = ftapi.GetEndpoint("skills/"+strconv.Itoa(skill.ID), nil)
+	skill.req.ExecuteMethod = func() {
+		skill.req.Patch(ctx, ftapi.EncapsulatedMarshal("skill", params))
+	}
+	return &skill.req
+}
+
+func (skill *Skill) Get(ctx context.Context) ftapi.CachedRequest {
+	skill.req.Endpoint = ftapi.GetEndpoint("skills/"+strconv.Itoa(skill.ID), nil)
+	skill.req.ExecuteMethod = func() {
+		skill.req.Get(ctx, skill)
+	}
+	return &skill.req
+}
+
+func (skills *Skills) GetAll(ctx context.Context) ftapi.CollectionRequest {
+	skills.req.Endpoint = ftapi.GetEndpoint("skills", nil)
+	skills.req.ExecuteMethod = func() {
+		skills.req.GetAll(ctx, &skills.Collection)
+	}
+	return &skills.req
+}
