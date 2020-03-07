68d5dd12cc8d6cb180919cdf0053a7c342dd4820
Stephen Gardner <stephenbgardner@gmail.com>
2020-03-06 16:18:54 -0800

    Add Languages Users

---
+++ ftobj/languages_users.go
@@ -0,0 +1,67 @@
+package ftobj
+
+import (
+	"context"
+	"intra/ftapi"
+	"strconv"
+)
+
+type (
+	LanguagesUser struct {
+		req        ftapi.RequestData
+		ID         int        `json:"id"`
+		LanguageID int        `json:"language_id"`
+		UserID     int        `json:"user_id"`
+		Position   int        `json:"position"`
+		CreatedAt  ftapi.Time `json:"created_at"`
+	}
+	LanguagesUsers struct {
+		req        ftapi.RequestData
+		Collection []LanguagesUser
+	}
+	LanguagesUserCUParams struct {
+		UserID     int `json:"user_id,omitempty"`
+		LanguageID int `json:"language_id,omitempty"`
+		Position   int `json:"position,omitempty"`
+	}
+)
+
+func (lu *LanguagesUser) Create(ctx context.Context, params LanguagesUserCUParams) ftapi.CachedRequest {
+	lu.req.Endpoint = ftapi.GetEndpoint("languages_users", nil)
+	lu.req.ExecuteMethod = func() {
+		lu.req.Create(ctx, lu, ftapi.EncapsulatedMarshal("languages_user", params))
+	}
+	return &lu.req
+}
+
+func (lu *LanguagesUser) Delete(ctx context.Context) ftapi.Request {
+	lu.req.Endpoint = ftapi.GetEndpoint("languages_users/"+strconv.Itoa(lu.ID), nil)
+	lu.req.ExecuteMethod = func() {
+		lu.req.Delete(ctx, lu)
+	}
+	return &lu.req
+}
+
+func (lu *LanguagesUser) Patch(ctx context.Context, params LanguagesUserCUParams) ftapi.Request {
+	lu.req.Endpoint = ftapi.GetEndpoint("languages_users/"+strconv.Itoa(lu.ID), nil)
+	lu.req.ExecuteMethod = func() {
+		lu.req.Patch(ctx, ftapi.EncapsulatedMarshal("languages_user", params))
+	}
+	return &lu.req
+}
+
+func (lu *LanguagesUser) Get(ctx context.Context) ftapi.CachedRequest {
+	lu.req.Endpoint = ftapi.GetEndpoint("languages_users/"+strconv.Itoa(lu.ID), nil)
+	lu.req.ExecuteMethod = func() {
+		lu.req.Get(ctx, lu)
+	}
+	return &lu.req
+}
+
+func (lus *LanguagesUsers) GetAll(ctx context.Context) ftapi.CollectionRequest {
+	lus.req.Endpoint = ftapi.GetEndpoint("languages_users", nil)
+	lus.req.ExecuteMethod = func() {
+		lus.req.GetAll(ctx, &lus.Collection)
+	}
+	return &lus.req
+}
